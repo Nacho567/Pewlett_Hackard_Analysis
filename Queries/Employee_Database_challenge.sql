5db36fa5c88e7cd0f9d365b8f3c43161b1346908
Ethan N <ethan.norberg@gmail.com>
2023-01-07 22:42:27 -0600

    Finished deliverables 2/3

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -68,3 +68,19 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,  
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY rt.emp_no ASC, rt.to_date DESC;
+
+--Retrieving the number of employees about to retire by most recent job title
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY (ut.title)
+ORDER BY COUNT(ut.title) DESC;
