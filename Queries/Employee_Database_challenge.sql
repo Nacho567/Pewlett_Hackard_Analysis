4693d0267acc8763b3a2cd472c3e441d654732c8
Ethan N <ethan.norberg@gmail.com>
2023-01-07 22:46:19 -0600

    saved SQL DB after deliverable 2 finished

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -54,6 +54,8 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no, from_date)
 );
 
+------Deliverable 1------
+
 --Joining tables to make Retirement Title table
 SELECT e.emp_no,
 	e.first_name,
@@ -84,3 +86,23 @@ INTO retiring_titles
 FROM unique_titles AS ut
 GROUP BY (ut.title)
 ORDER BY COUNT(ut.title) DESC;
+
+------Deliverable 2------
+
+--Create a mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+LEFT OUTER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+LEFT OUTER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
